9d4440a22bf1e36663c6c997c96e46a462564292
hungkr01 <luonghung1005@gmail.com>
2021-11-15 11:02:13 +0700

    dat

--- store/models.py
+++ store/models.py
@@ -14,7 +14,7 @@ class Customer(models.Model):
 
 class Product(models.Model):
 	name = models.CharField(max_length=200)
-	price = models.FloatField()
+	price = models.BigIntegerField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
 	image = models.ImageField(null=True, blank=True)
 
